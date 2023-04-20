d70933126546f9d94273a961213804af22f3d71c
nikosev <nikos.ev@hotmail.com>
2023-04-20 15:42:11 +0300

    Remove non static global variables

--- egi-check-in-validator.py
+++ egi-check-in-validator.py
@@ -4,10 +4,6 @@ import os
 import select
 import sys
 
-# ISSUER = ""
-UNIQUE_IDENTIFIER = ""
-GROUPS = []
-SCOPES = []
 TIMEOUT = 5
 CONFIG_PATHS = [
     "/etc/condor-ce.d/egi-check-in-validator.conf"
@@ -63,72 +59,59 @@ def valid_lines(file):
 
 # Get environment variables
 def parse_env_variables():
-    # global ISSUER
-    global UNIQUE_IDENTIFIER
-    global GROUPS
-    global SCOPES
-    # ISSUER = os.getenv("BEARER_TOKEN_0_CLAIM_iss_0")
+    # issuer = os.getenv("BEARER_TOKEN_0_CLAIM_iss_0")
     unique_id = os.getenv("BEARER_TOKEN_0_CLAIM_voperson_id_0")
     if unique_id is None:
         unique_id = os.getenv("BEARER_TOKEN_0_CLAIM_sub_0")
-    UNIQUE_IDENTIFIER = unique_id
-    inx = 0
+    idx = 0
     eduperson_entitlement = []
     while True:
-        group = os.getenv("BEARER_TOKEN_0_CLAIM_eduperson_entitlement_" + str(inx))
+        group = os.getenv("BEARER_TOKEN_0_CLAIM_eduperson_entitlement_" + str(idx))
         if group is None:
             break
         eduperson_entitlement.append(group)
-        inx += 1
-    GROUPS = eduperson_entitlement
-    inx = 0
+        idx += 1
+    idx = 0
     scopes = []
     while True:
-        group = os.getenv("BEARER_TOKEN_0_SCOPE_" + str(inx))
+        group = os.getenv("BEARER_TOKEN_0_SCOPE_" + str(idx))
         if group is None:
             break
         scopes.append(group)
-        inx += 1
-    SCOPES = scopes
+        idx += 1
+    return unique_id, groups, scopes
 
 
 def parse_jwt(jwt_string):
-    # global ISSUER
-    global UNIQUE_IDENTIFIER
-    global GROUPS
-    global SCOPES
     jwt = json.loads(jwt_string)
-    # ISSUER = jwt["iss"]
+    # issuer = jwt["iss"]
     unique_id = ""
     if "voperson_id" in jwt:
         unique_id = jwt["voperson_id"]
     elif "sub" in jwt:
         unique_id = jwt["sub"]
-    UNIQUE_IDENTIFIER = unique_id
     if "eduperson_entitlement" in jwt:
-        GROUPS = jwt["eduperson_entitlement"]
+        groups = jwt["eduperson_entitlement"]
     if "scope" in jwt:
-        SCOPES = jwt["scope"].split()
+        scopes = jwt["scope"].split()
+    return unique_id, groups, scopes
 
 
-def process_jwt():
-    global GROUPS
-    global SCOPES
-
+def process_jwt(groups, scopes):
     entitlements = []
-    if SCOPES and not GROUPS:
-        for item in SCOPES:
+    if scopes and not groups:
+        for item in scopes:
             if "eduperson_entitlement:" in item:
                 entitlements.append(item.replace("eduperson_entitlement:", ""))
             # TEMP
             if "eduperson_entitlement_jwt:" in item:
                 entitlements.append(item.replace("eduperson_entitlement_jwt:", ""))
-        GROUPS = entitlements
+        groups = entitlements
 
-    inx = 0
-    while inx < len(GROUPS):
-        os.environ["BEARER_TOKEN_0_GROUP_" + str(inx)] = GROUPS[inx]
-        inx += 1
+    idx = 0
+    while idx < len(groups):
+        os.environ["BEARER_TOKEN_0_GROUP_" + str(idx)] = groups[idx]
+        idx += 1
     sys.exit(0)
 
 
@@ -138,10 +121,10 @@ if __name__ == "__main__":
     print("Insert JWT: ")
     i, o, e = select.select([sys.stdin], [], [], TIMEOUT)
     if i:
-        parse_jwt(sys.stdin.readline().strip())
+        unique_id, groups, scopes = parse_jwt(sys.stdin.readline().strip())
     else:
-        parse_env_variables()
-    if GROUPS or SCOPES:
-        process_jwt()
+        unique_id, groups, scopes = parse_env_variables()
+    if groups or scopes:
+        process_jwt(groups, scopes)
     else:
         sys.exit(1)
